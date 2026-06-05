a2551136597174fdac74752166bfab93e541ea7a
Ubuntu <ubuntu@ip-172-31-47-70.ap-south-1.compute.internal>
2026-06-05 08:34:04 +0000

    Initial Tirupati Temple App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>🛕 Tirupati Temple</h1>
+
+      <img
+        src="/images/tirupati.jpg"
+        width="450"
+        style={{ borderRadius: "15px" }}
+        alt="Tirupati"
+      />
+
+      <p>🙏 Sri Venkateswara Swamy Temple</p>
     </div>
   );
 }
